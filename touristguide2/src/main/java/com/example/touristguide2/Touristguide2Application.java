984b497060af1cc7ffafbcc21aac6d54a17ed08e
Mona Taherzadeh <147496421+monataherzadeh@users.noreply.github.com>
2025-09-03 10:33:38 +0200

    Add files via upload

---
+++ touristguide2/src/main/java/com/example/touristguide2/Touristguide2Application.java
@@ -0,0 +1,13 @@
+package com.example.touristguide2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Touristguide2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Touristguide2Application.class, args);
+	}
+
+}
