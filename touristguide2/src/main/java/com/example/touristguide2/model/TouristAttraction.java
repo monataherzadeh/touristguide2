8b38a123b3cc43b591080f14e3a6f58b25d0dbee
404BrainNotFound1000 <emilgurres@gmail.com>
2025-09-15 23:27:18 +0200

    html document attractionList.html finished. Still failing to execute the showAllAttracitons() controller method.

--- touristguide2/src/main/java/com/example/touristguide2/model/TouristAttraction.java
+++ touristguide2/src/main/java/com/example/touristguide2/model/TouristAttraction.java
@@ -26,6 +26,9 @@ public class TouristAttraction {
         return description;
 
     }
+    public String getLocation() {
+        return location;
+    }
 
     public void setLocation(String location) {
         this.location = location;
